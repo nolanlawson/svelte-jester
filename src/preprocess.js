a38d68b6e0f1cc2a80dd8a7e93fd46f6d85acc3b
Nolan Lawson <nolan@nolanlawson.com>
2020-06-10 07:49:08 -0700

    fix: auto-preprocess instead of loading svelte.config.js

--- src/preprocess.js
+++ src/preprocess.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const svelte = require('svelte/compiler')
-const { cosmiconfigSync } = require('cosmiconfig')
 
 const { source, filename } = process.env
-const configPath = path.join(process.cwd(), 'svelte.config.js')
-const config = cosmiconfigSync().load(configPath).config
+
+// TODO: loading svelte.config.js fails if you are in "type":"module"
+// So just load svelte-preprocess directly
+const preprocess = require('svelte-preprocess')
 
 svelte
-  .preprocess(source, config.preprocess || {}, { filename })
+  .preprocess(source, preprocess(), { filename })
   .then(r => { process.stdout.write(r.code) })
